bd7ed6dc29f07d1851b4d8be21f1fbaedf344306
Adam <a.p.bates@hotmail.co.uk>
2017-10-23 15:04:37 +0100

    External Panel

--- www/js/index.js
+++ www/js/index.js
@@ -8,27 +8,9 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 // device APIs are available
 //
-    function onDeviceReady() {
-	    
-	console.log("device ready");
-        var person= {
-		"firstName": "Homer",
-  		"lastName": "Simpson",
- 		"address": {
-    			"streetAddress": "742 Evergreen Terrace",
-    			"city": "Springfield",
-    			"state": "?"
-		}
-	}
-
-        storeItem("person", JSON.stringify(person));
-        storeItem("hair", "brown");
-        storeItem("beard", "messy");
-        storeItem("weather", "weird");
-        storeItem("date", "16th oct 2017");
-        
-        document.getElementById("storage1").innerHTML = "First name: " + JSON.parse(getItem("person")).firstName;
-        document.getElementById("storage2").innerHTML = "Storage items: " + window.localStorage.length;
+    function onDeviceReady() { 
+	   console.log("device ready");
+       $('#mypanel').enhanceWithin().panel();
     }
 
 
